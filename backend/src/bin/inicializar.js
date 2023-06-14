57244273ec7abf1cf6c9c5339a3eebf4294ea2a4
erick.jose.heiler@gmail.com <erick.jose.heiler@gmail.com>
2023-06-14 14:17:38 -0300

    t

--- backend/src/bin/inicializar.js
+++
@@ -1,9 +0,0 @@
-import app from '../app.js'
-
-// inicializa o servidor
-app.listen(3000, () => {
-    console.log('Servidor iniciado na porta 3000: http://localhost:3000/');
-});
-
-// para inicializar:
-// npm run dev
